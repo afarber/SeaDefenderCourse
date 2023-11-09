5801abf6cb730c0e4828a5eb8bd66e1adfe4ec43
Alexander Farber <farber72@outlook.de>
2023-11-09 18:34:18 +0100

    Have 2 right and 3 left shark spawner scenes

--- enemies/enemy_spawner/enemy_spawner.gd
+++ enemies/enemy_spawner/enemy_spawner.gd
@@ -1,5 +1,7 @@
 extends Marker2D
 
+@export var facing_left = false
+
 const Shark = preload("res://enemies/shark/shark.tscn")
 
 func _on_timer_timeout():
@@ -7,3 +9,5 @@ func _on_timer_timeout():
 	get_tree().current_scene.add_child(shark_instance)
 	# set the new spawned shark position to the position of the marker cross
 	shark_instance.global_position = global_position
+	if facing_left:
+		shark_instance.flip_direction()
--- enemies/shark/shark.gd
+++ enemies/shark/shark.gd
@@ -6,6 +6,8 @@ const MOVEMENT_AMPLITUDE = 0.5
 
 var velocity = Vector2(1, 0)
 
+@onready var sprite = $AnimatedSprite2D
+
 func _physics_process(delta):
 	velocity.y = sin(global_position.x * MOVEMENT_FREQUENCY) * MOVEMENT_AMPLITUDE
 	global_position += velocity * SPEED * delta
@@ -15,6 +17,10 @@ func _on_area_entered(area):
 		# the parent of the area is player_bullet
 		area.queue_free()
 		queue_free()
+	
+func flip_direction():
+	velocity = -velocity
+	sprite.flip_h = !sprite.flip_h
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
