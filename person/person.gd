bd5e975b343f1c5705c93e9250cb71fce74cd114
Alexander Farber <farber72@outlook.de>
2023-11-09 18:34:18 +0100

    Emit a custom update_people_count signal

--- person/person.gd
+++ person/person.gd
@@ -12,6 +12,7 @@ func _physics_process(delta):
 func _on_area_entered(area):
 	if area.is_in_group("PlayerGroup"):
 		Global.saved_people_count += 1
+		GameEvent.emit_signal("update_people_count")
 		queue_free()
 	elif area.is_in_group("PlayerBulletGroup"):
 		queue_free()
--- user_interface/people-count/person_ui.gd
+++ user_interface/people-count/person_ui.gd
@@ -5,5 +5,8 @@ extends Sprite2D
 const EMPTY_TEXTURE = preload("res://user_interface/people-count/person_empty_ui.png")
 const FULL_TEXTURE = preload("res://user_interface/people-count/person_ui.png")
 
-func _process(delta):
+func _ready():
+	GameEvent.connect("update_people_count", Callable(self, "_update"))
+	
+func _update():
 	texture = FULL_TEXTURE if Global.saved_people_count >= order_number else EMPTY_TEXTURE
