cc5573ba9a0de7efcbe2ea753ea95ebfc74eecf0
Alexander Farber <farber72@outlook.de>
2023-11-09 18:34:17 +0100

    Introduce current_angle, target_angle (0 or PI), does not work yet

--- player/player.gd
+++ player/player.gd
@@ -4,9 +4,11 @@ var can_shoot = true
 var velocity = Vector2(0, 0)
 
 const TURNING_START = 1
-var is_turning_left = false
-var is_turning_right = false
 var turning_timer = TURNING_START
+# if player looks left, the angle is 0
+# if player looks right, the angle is PI
+var current_angle = 0
+var target_angle = 0
 
 const SPEED = Vector2(125, 90)
 const BULLET_OFFSET = 7
@@ -19,39 +21,27 @@ func _process(delta):
 	velocity.x = Input.get_axis("move_left", "move_right")	
 	velocity.y = Input.get_axis("move_up", "move_down")
 	velocity = velocity.normalized()
-
-	if is_turning_left:
-		turning_timer -= delta
-		print("Updated turning_timer=%f" % turning_timer)
-		scale.x = 0.5 #turning_timer / TURNING_START
-		if turning_timer <= 0:
-			is_turning_left = false
-			is_turning_right = false
-			flip_h = true
-			scale.x = 1
-	elif is_turning_right:
-		turning_timer -= delta
-		print("Updated turning_timer=%f" % turning_timer)
-		scale.x = 0.5 #turning_timer / TURNING_START
-		if turning_timer <= 0:
-			is_turning_left = false
-			is_turning_right = false
-			flip_h = false
-			scale.x = 1
 	
-	if velocity.x > 0 and !is_turning_right:
-		is_turning_left = false
-		is_turning_right = true
+	if turning_timer > 0:
+		turning_timer -= delta
+		print("Updated turning_timer=%f" % turning_timer)
+		# arrive at the target, stop the timer
+		if turning_timer <= 0:
+			turning_timer == 0
+			current_angle = target_angle
+			scale.x = 1
+		else:
+			current_angle = target_angle - turning_timer / TURNING_START
+			scale.x = cos(current_angle)
+	
+	if velocity.x > 0 and current_angle == PI:
+		target_angle = 0
 		turning_timer = TURNING_START
 		print("Started turning_timer=%f" % turning_timer)
-	elif velocity.x < 0 and !is_turning_left:
-		is_turning_left = true
-		is_turning_right = false
+	elif velocity.x < 0 and current_angle == 0:
+		target_angle = PI
 		turning_timer = TURNING_START
 		print("Started turning_timer=%f" % turning_timer)
-		
-	# TODO
-	#scale.x = .5
 	
 	if Input.is_action_pressed("shoot") and can_shoot:
 		var bullet_instance = Bullet.instantiate()
