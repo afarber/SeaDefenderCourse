27d79ace4d28b8e29034cabc50159a08668dd19f
Alexander Farber <farber72@outlook.de>
2023-11-09 18:34:18 +0100

    Destroy sharks exiting the screen by using VisibleOnScreenNotifier2D

--- enemies/shark/shark.gd
+++ enemies/shark/shark.gd
@@ -15,3 +15,6 @@ func _on_area_entered(area):
 		# the parent of the area is player_bullet
 		area.queue_free()
 		queue_free()
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
