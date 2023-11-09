6562740c1c8ca9651a2b4faf85da4083aef16f51
Alexander Farber <farber72@outlook.de>
2023-11-09 18:34:18 +0100

    Ensure that sharks do not collide

--- enemies/enemy_spawner/enemy_spawner.gd
+++ enemies/enemy_spawner/enemy_spawner.gd
@@ -2,17 +2,27 @@ extends Node2D
 
 const Shark = preload("res://enemies/shark/shark.tscn")
 
+var used_spawn_points = []
+
 @onready var left = $Left
 @onready var right = $Right
 
 func _on_spawn_enemy_timer_timeout():
+	used_spawn_points.clear()
 	for i in range(4):
 		spawn_enemy()
 		
 func spawn_enemy():
+	var available_spawn_points = []
+	for i in range(1, 5):
+		if !used_spawn_points.has(i):
+			available_spawn_points.append(i)
+
+	var rand_spawn_point_index = randi_range(0, available_spawn_points.size() - 1)
+	var selected_spawn_point_number = available_spawn_points[rand_spawn_point_index]
+	used_spawn_points.append(selected_spawn_point_number)
 	var selected_side_node = left if bool(randi_range(0, 1)) else right
-	var rand_spawn_point_number = randi_range(1, 4)
-	var selected_spawn_point = selected_side_node.get_node(str(rand_spawn_point_number))
+	var selected_spawn_point = selected_side_node.get_node(str(selected_spawn_point_number))
 	var spawn_position = selected_spawn_point.global_position
 	
 	var shark_instance = Shark.instantiate()
