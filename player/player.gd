e2ea9f99da7abb609fe39bac3aa4e1dd4d5681f4
Alexander Farber <farber72@outlook.de>
2023-11-09 18:34:17 +0100

    Flipping works correctly

--- player/player.gd
+++ player/player.gd
@@ -7,7 +7,7 @@ const TURNING_TARGET = 2.0
 var turning_timer = TURNING_TARGET
 # if player looks left, the angle is 0
 # if player looks right, the angle is PI
-var current_angle = 0.0
+var start_angle = 0.0
 var target_angle = 0.0
 
 const SPEED = Vector2(125, 90)
@@ -28,24 +28,24 @@ func _process(delta):
 		# arrived at the target, stop the timer
 		if turning_timer >= TURNING_TARGET:
 			turning_timer = TURNING_TARGET
-			current_angle = target_angle
+			start_angle = target_angle
 			scale.x = 1
 		else:
 			# flip when passed half of the timer
 			if turning_timer >= TURNING_TARGET / 2:
 				flip_h = (target_angle == PI)
-			current_angle = lerp(current_angle, target_angle, turning_timer / TURNING_TARGET)
-			scale.x = abs(cos(current_angle))
-			print("UPDATED current_angle=%.2f -> target_angle=%.2f scale.x=%.2f flip_h=%s" % [current_angle, target_angle, scale.x, flip_h])
+			var angle = lerp(start_angle, target_angle, turning_timer / TURNING_TARGET)
+			scale.x = abs(cos(angle))
+			print("UPDATED angle=%.2f -> target_angle=%.2f scale.x=%.2f flip_h=%s" % [angle, target_angle, scale.x, flip_h])
 			
-	if velocity.x > 0 and current_angle == PI:
+	if velocity.x > 0 and target_angle == PI:
 		target_angle = 0.0
 		turning_timer = 0.0
-		print("STARTED current_angle=%.2f -> target_angle=%.2f scale.x=%.2f flip_h=%s" % [current_angle, target_angle, scale.x, flip_h])
-	elif velocity.x < 0 and current_angle == 0.0:
+		print("STARTED start_angle=%.2f -> target_angle=%.2f scale.x=%.2f flip_h=%s" % [start_angle, target_angle, scale.x, flip_h])
+	elif velocity.x < 0 and target_angle == 0.0:
 		target_angle = PI
 		turning_timer = 0.0
-		print("STARTED current_angle=%.2f -> target_angle=%.2f scale.x=%.2f flip_h=%s" % [current_angle, target_angle, scale.x, flip_h])
+		print("STARTED start_angle=%.2f -> target_angle=%.2f scale.x=%.2f flip_h=%s" % [start_angle, target_angle, scale.x, flip_h])
 	
 	if Input.is_action_pressed("shoot") and can_shoot:
 		var bullet_instance = Bullet.instantiate()
