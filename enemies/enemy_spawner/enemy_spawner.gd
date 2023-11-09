2e980c7b40ab7f50c5b0b987e4aba9ba382931c8
Alexander Farber <farber72@outlook.de>
2023-11-09 18:34:18 +0100

    Spawn sharks at the marker

--- enemies/enemy_spawner/enemy_spawner.gd
+++ enemies/enemy_spawner/enemy_spawner.gd
@@ -1,11 +1,9 @@
 extends Marker2D
 
+const Shark = preload("res://enemies/shark/shark.tscn")
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_timer_timeout():
+	var shark_instance = Shark.instantiate()
+	get_tree().current_scene.add_child(shark_instance)
+	# set the new spawned shark position to the position of the marker cross
+	shark_instance.global_position = global_position
