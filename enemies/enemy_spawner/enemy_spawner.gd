21a954e212090d08c9ace3abfe5666f636142e36
Alexander Farber <farber72@outlook.de>
2023-11-09 18:34:18 +0100

    Spawn a person at one of the 8 markers

--- enemies/enemy_spawner/enemy_spawner.gd
+++ enemies/enemy_spawner/enemy_spawner.gd
@@ -12,20 +12,27 @@ func _on_spawn_enemy_timer_timeout():
 	while available_spawn_points.size() > 0:
 		var selected_spawn_point_number = available_spawn_points.pop_front()
 		spawn_enemy(selected_spawn_point_number)
-		
+
 func spawn_enemy(selected_spawn_point_number):
 	var selected_side_node = left if bool(randi_range(0, 1)) else right
 	var selected_spawn_point = selected_side_node.get_node(str(selected_spawn_point_number))
 	var spawn_position = selected_spawn_point.global_position
-	
+
 	var shark_instance = Shark.instantiate()
 	shark_instance.global_position = spawn_position
 	get_tree().current_scene.add_child(shark_instance)
-	
+
 	if selected_side_node == right:
 		shark_instance.flip_direction()
 
 func _on_spawn_person_timer_timeout():
 	var person_instance = Person.instantiate()
-	person_instance.global_position = Vector2(10, 20)
 	get_tree().current_scene.add_child(person_instance)
+	var selected_spawn_point_number = randi_range(1, 4)
+	var selected_side_node = left if bool(randi_range(0, 1)) else right
+	var selected_spawn_point = selected_side_node.get_node(str(selected_spawn_point_number))
+	var spawn_position = selected_spawn_point.global_position
+	person_instance.global_position = spawn_position
+
+	if selected_side_node == right:
+		person_instance.flip_direction()
--- person/person.gd
+++ person/person.gd
@@ -2,6 +2,8 @@ extends Area2D
 
 var velocity = Vector2(1, 0)
 
+@onready var sprite = $AnimatedSprite2D
+
 const SPEED = 25
 
 func _physics_process(delta):
@@ -14,6 +16,9 @@ func _on_area_entered(area):
 	elif area.is_in_group("PlayerBulletGroup"):
 		queue_free()
 
+func flip_direction():
+	velocity = -velocity
+	sprite.flip_h = !sprite.flip_h
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
