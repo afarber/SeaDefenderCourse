7d147dbb4e13c663fd4fbce5919ccdc3f0814d03
Alexander Farber <farber72@outlook.de>
2023-11-09 18:34:18 +0100

    Remove start_angle

--- player/player.gd
+++ player/player.gd
@@ -7,7 +7,6 @@ const TURNING_TARGET = 2.0
 var turning_timer = TURNING_TARGET
 # if player looks left, the angle is 0
 # if player looks right, the angle is PI
-var start_angle = 0.0
 var target_angle = 0.0
 
 const SPEED = Vector2(125, 90)
@@ -22,30 +21,33 @@ func _process(delta):
 	velocity.y = Input.get_axis("move_up", "move_down")
 	velocity = velocity.normalized()
 	
+	# if the timer is active
 	if turning_timer < TURNING_TARGET:
 		turning_timer += delta
 
-		# arrived at the target, stop the timer
+		# completed turning, stop the timer
 		if turning_timer >= TURNING_TARGET:
 			turning_timer = TURNING_TARGET
-			start_angle = target_angle
 			scale.x = 1
 		else:
-			# flip when passed half of the timer
+			# flip to the moving direction when passed half of the timer
 			if turning_timer >= TURNING_TARGET / 2:
 				flip_h = (target_angle == PI)
+			var start_angle = PI if target_angle == 0.0 else 0.0
 			var angle = lerp(start_angle, target_angle, turning_timer / TURNING_TARGET)
 			scale.x = abs(cos(angle))
 			print("UPDATED angle=%.2f -> target_angle=%.2f scale.x=%.2f flip_h=%s" % [angle, target_angle, scale.x, flip_h])
-			
-	if velocity.x > 0 and target_angle == PI:
-		target_angle = 0.0
-		turning_timer = 0.0
-		print("STARTED start_angle=%.2f -> target_angle=%.2f scale.x=%.2f flip_h=%s" % [start_angle, target_angle, scale.x, flip_h])
-	elif velocity.x < 0 and target_angle == 0.0:
-		target_angle = PI
-		turning_timer = 0.0
-		print("STARTED start_angle=%.2f -> target_angle=%.2f scale.x=%.2f flip_h=%s" % [start_angle, target_angle, scale.x, flip_h])
+	else:
+		# timer not active and moving right, but facing left
+		if velocity.x > 0 and flip_h:
+			target_angle = 0.0
+			turning_timer = 0.0
+			print("STARTED target_angle=%.2f scale.x=%.2f flip_h=%s" % [target_angle, scale.x, flip_h])
+		# timer not active and moving left, but facing right
+		elif velocity.x < 0 and !flip_h:
+			target_angle = PI
+			turning_timer = 0.0
+			print("STARTED target_angle=%.2f scale.x=%.2f flip_h=%s" % [target_angle, scale.x, flip_h])
 	
 	if Input.is_action_pressed("shoot") and can_shoot:
 		var bullet_instance = Bullet.instantiate()
