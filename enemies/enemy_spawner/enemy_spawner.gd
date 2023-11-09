feca7f2602474e5a079434f20752d5d593e2bf13
Alexander Farber <farber72@outlook.de>
2023-11-09 18:34:18 +0100

    Introduce spawn_instance() to spawn a shark or a person at one of the 8 markers

--- enemies/enemy_spawner/enemy_spawner.gd
+++ enemies/enemy_spawner/enemy_spawner.gd
@@ -10,29 +10,22 @@ func _on_spawn_enemy_timer_timeout():
 	var available_spawn_points = range(1, 5)
 	available_spawn_points.shuffle()
 	while available_spawn_points.size() > 0:
+		var shark_instance = Shark.instantiate()
 		var selected_spawn_point_number = available_spawn_points.pop_front()
-		spawn_enemy(selected_spawn_point_number)
+		spawn_instance(shark_instance, selected_spawn_point_number)
 
-func spawn_enemy(selected_spawn_point_number):
+func spawn_instance(enemy_or_person_instance, selected_spawn_point_number):
 	var selected_side_node = left if bool(randi_range(0, 1)) else right
 	var selected_spawn_point = selected_side_node.get_node(str(selected_spawn_point_number))
 	var spawn_position = selected_spawn_point.global_position
 
-	var shark_instance = Shark.instantiate()
-	shark_instance.global_position = spawn_position
-	get_tree().current_scene.add_child(shark_instance)
+	enemy_or_person_instance.global_position = spawn_position
+	get_tree().current_scene.add_child(enemy_or_person_instance)
 
 	if selected_side_node == right:
-		shark_instance.flip_direction()
+		enemy_or_person_instance.flip_direction()
 
 func _on_spawn_person_timer_timeout():
 	var person_instance = Person.instantiate()
-	get_tree().current_scene.add_child(person_instance)
 	var selected_spawn_point_number = randi_range(1, 4)
-	var selected_side_node = left if bool(randi_range(0, 1)) else right
-	var selected_spawn_point = selected_side_node.get_node(str(selected_spawn_point_number))
-	var spawn_position = selected_spawn_point.global_position
-	person_instance.global_position = spawn_position
-
-	if selected_side_node == right:
-		person_instance.flip_direction()
+	spawn_instance(person_instance, selected_spawn_point_number)
