d3348c15a3905d35d83f1fba07b160575f40efc2
Alexander Farber <farber72@outlook.de>
2023-11-09 18:34:17 +0100

    Add reload timer and listen for it signal

--- player/player.gd
+++ player/player.gd
@@ -1,8 +1,12 @@
 extends AnimatedSprite2D
 
+var can_shoot = true
 var velocity = Vector2(0, 0)
+
 const SPEED = Vector2(125, 90)
 const Bullet = preload("res://player/player_bullet/player_bullet.tscn")
+
+@onready var reload_timer = $ReloadTimer
 
 func _process(delta):
 	velocity.x = Input.get_axis("move_left", "move_right")	
@@ -15,13 +19,18 @@ func _process(delta):
 	elif velocity.x < 0:
 		flip_h = true
 		
+	# TODO
 	#scale.x = .5
-	if Input.is_action_just_pressed("shoot"):
+	
+	if Input.is_action_pressed("shoot") and can_shoot:
 		var bullet_instance = Bullet.instantiate()
 		bullet_instance.global_position = global_position
 		get_tree().current_scene.add_child(bullet_instance)
-
+		can_shoot = false
+		reload_timer.start()
 	
 func _physics_process(delta):
 	global_position += velocity * SPEED * delta
 	
+func _on_reload_timer_timeout():
+	can_shoot = true
