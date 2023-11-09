51d58332255a30211c539cb821bf751ee6362ec4
Alexander Farber <farber72@outlook.de>
2023-11-09 18:34:18 +0100

    Remove person scene when it collides with player or a bullet

--- person/person.gd
+++ person/person.gd
@@ -6,4 +6,10 @@ const SPEED = 25
 
 func _physics_process(delta):
 	global_position += velocity * SPEED * delta
-	
+
+func _on_area_entered(area):
+	if area.is_in_group("PlayerGroup"):
+		# increase crew counter
+		queue_free()
+	elif area.is_in_group("PlayerBulletGroup"):
+		queue_free()
