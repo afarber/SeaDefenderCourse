0637cf0c2434bff41ffcb77c9bc5ee2d70847fae
Alexander Farber <farber72@outlook.de>
2023-11-09 18:34:18 +0100

    Make the player scene Area2D

--- player/player.gd
+++ player/player.gd
@@ -1,4 +1,4 @@
-extends AnimatedSprite2D
+extends Area2D
  
 var can_shoot = true
 var velocity = Vector2(0, 0)
@@ -10,7 +10,8 @@ var going_left = false
 const SPEED = Vector2(125, 90)
 const BULLET_OFFSET = 7
 const Bullet = preload("res://player/player_bullet/player_bullet.tscn")
- 
+
+@onready var sprite = $AnimatedSprite2D
 @onready var reload_timer = $ReloadTimer
  
 func _process(delta):
@@ -24,7 +25,7 @@ func _process(delta):
 	if Input.is_action_pressed("shoot") and can_shoot:
 		var bullet_instance = Bullet.instantiate()
 		get_tree().current_scene.add_child(bullet_instance)
-		if flip_h:
+		if sprite.flip_h:
 			bullet_instance.flip_direction()
 			bullet_instance.global_position = global_position - Vector2(BULLET_OFFSET, 0)
 		else:
@@ -51,7 +52,7 @@ func _flip_the_player(velocity_x, delta):
 		else:
 			# flip to the moving direction when passed half of the timer
 			if flip_seconds >= FLIP_DURATION / 2:
-				flip_h = going_left
+				sprite.flip_h = going_left
 			# if player looks left, the angle is PI
 			# if player looks right, the angle is 0
 			var start_angle = 0.0 if going_left else PI
@@ -60,10 +61,10 @@ func _flip_the_player(velocity_x, delta):
 			scale.x = abs(cos(angle))
 	else:
 		# animation not active and moving right, but facing left
-		if velocity_x > 0 and flip_h:
+		if velocity_x > 0 and sprite.flip_h:
 			going_left = false
 			flip_seconds = 0.0
 		# animation not active and moving left, but facing right
-		elif velocity_x < 0 and !flip_h:
+		elif velocity_x < 0 and !sprite.flip_h:
 			going_left = true
 			flip_seconds = 0.0
