13e35766149d7053d2bbdfca72b353b4e5b3c179
Alexander Farber <farber72@outlook.de>
2023-11-11 17:36:23 +0100

    Display high score in the game over screen and restart the game when player shoots again

--- global/global.gd
+++ global/global.gd
@@ -3,6 +3,7 @@ extends Node
 var saved_people_count = 0
 var oxygen_level = 100
 var current_points = 0
+var high_score = 0
 
 # TODO add functions to update the above variables and emit signals
 
--- user_interface/game-over/game_over.gd
+++ user_interface/game-over/game_over.gd
@@ -1,11 +1,29 @@
 extends Control
 
 @onready var current_score_label = $CurrentScoreLabel
+@onready var high_score_label = $HighScoreLabel
+@onready var game_over_delay_timer = $GameOverDelay
 
 func _ready():
 	GameEvent.connect("game_over", Callable(self, "_activate_game_over"))
 	visible = false
 
+func _process(delta):
+	if visible and Input.is_action_just_pressed("shoot"):
+		Global.current_points = 0
+		Global.saved_people_count = 0
+		Global.oxygen_level = 100
+
+		get_tree().reload_current_scene()
+
 func _activate_game_over():
+	if Global.current_points > Global.high_score:
+		Global.high_score = Global.current_points
+
 	current_score_label.text = "Score " + str(Global.current_points)
+	high_score_label.text = "Highscore " + str(Global.high_score)
+
+	game_over_delay_timer.start()
+
+func _on_game_over_delay_timeout():
 	visible = true
