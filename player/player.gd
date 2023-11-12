bfe839837de1c9852199949fb89809d81ef686f1
Alexander Farber <farber72@outlook.de>
2023-11-12 19:32:58 +0100

    Delete the audio stream player every time the sound is finished

--- player/player.gd
+++ player/player.gd
@@ -13,6 +13,7 @@ const SPEED = Vector2(125, 90)
 const BULLET_OFFSET = 7
 
 const Bullet = preload("res://player/player_bullet/player_bullet.tscn")
+const ShootSound = preload("res://player/player_bullet/player_shoot.ogg")
 
 enum State { DEFAULT, PEOPLE_REFUEL, OXYGEN_REFUEL }
 var state = State.DEFAULT
@@ -92,6 +93,7 @@ func process_shooting():
 	if Input.is_action_pressed("shoot") and can_shoot:
 		var bullet_instance = Bullet.instantiate()
 		get_tree().current_scene.add_child(bullet_instance)
+		SoundManager.play_sound(ShootSound)
 		if sprite.flip_h:
 			bullet_instance.flip_direction()
 			bullet_instance.global_position = global_position - Vector2(BULLET_OFFSET, 0)
